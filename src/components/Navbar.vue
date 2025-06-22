26b765be12df781141c8bd14ed3b181b4be2db09
will <willwind0414@gmail.com>
2025-06-22 20:32:23 +0800

    git init

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,47 +1,30 @@
 <script setup lang="ts">
-// 定義一個 TypeScript 型別 NavItem，代表一個選單項目物件
+import { onMounted, ref, watchEffect } from 'vue'
+
 type NavItem = {
   id: string,
-  title: string //屬性，型別是字串
+  title: string
 }
 
-// 使用 defineProps() 宣告這個元件會接收一個 prop，名為 nav
-// 接收 nav 作為 props，為 NavItem 陣列
-const { nav, siteTitle } = defineProps<{
-  nav: NavItem[]
-  siteTitle: string
-}>()
+const props = defineProps<{ nav: NavItem[], siteTitle: string }>()
 
-/* 縮寫
-defineProps<{
-  nav: { title: string }[]
-}>()
-*/
+const activeId = ref('')
+const isMenuOpen = ref(false)
 
-/*切換選單, 改變樣式*/
-import { onMounted, ref } from 'vue'
-
-// 定義目前選中的選單 id
-const activeId = ref<string>('')
-
-// 點選切換 active
 const setActive = (id: string) => {
   activeId.value = id
 }
-
-const isMenuOpen = ref(false)
 
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value
 }
 
-
 /* ------------------------
-  生命周期：mounted
+  生命周期：
 -------------------------*/
-onMounted(() => {
-  if (nav && nav.length > 0 && nav[0]?.id) {
-    activeId.value = nav[0].id
+watchEffect(() => {
+  if (props.nav.length && !activeId.value) {
+    activeId.value = props.nav[0].id
   }
 })
 </script>
